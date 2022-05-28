da7d14ec6ebb3fc7f60361734db7dd77b322d69f
franGuindon <fbadilla10@gmail.com>
2022-05-27 22:30:15 -0600

    Programa_clasificador_objetos/main.py: Agrega implementación para hacer stepping del motor.

--- Programa_clasificador_objetos/main.py
+++ Programa_clasificador_objetos/main.py
@@ -1,7 +1,7 @@
 import time
 
 from arg_parser import parse_args
-#from gpio import Pin, cleanup as gpio_cleanup
+from gpio import Pin, cleanup as gpio_cleanup
 from obj_classifier import \
     setup as classifier_setup, \
     classify_object, try_classify_object, \
@@ -14,13 +14,25 @@ class Belt:
         self.setup_pins()
 
     def setup_pins(self):
-        self.belt_motor = Pin(0)
-        self.arm_motor = Pin(1)
+        self.belt_motor_pins = Pin(13), Pin(11), Pin(15), Pin(12)
+        #self.arm_motor = Pin(1)
+
+    def write_belt(self, seq):
+        a,b,c,d = seq
+        seq = int(a), int(b), int(c), int(d)
+        for val, pin in zip(seq, self.belt_motor_pins):
+            if val: pin.high()
+            else: pin.low()
 
     def step_belt(self):
-        self.belt_motor.high()
-        time.sleep(0.2)
-        self.belt_motor.low()
+        seq = ["1000","1100","0100","0110","0010","0011","0001","1001"]
+        direction = self.args.step_count > 0
+        if direction: index_range = range(0, self.args.step_count, 1)
+        else: index_range = range(-self.args.step_count, 0, -1)
+        for i in index_range:
+            elem = seq[i]
+            self.write_belt(elem)
+            time.sleep(self.args.velocity)
 
     def place_object_at_camera(self):
         grabbed = False
@@ -28,22 +40,24 @@ class Belt:
             self.step_belt()
             grabbed, self.obj_class = try_classify_object()
 
-    def classify_object(self):
-        obj_class = classify_oject()
+    def classify_object(self): pass
     
     def run(self):
         self.running = True
         while self.running:
             self.place_object_at_camera()
             self.classify_object()
+    
+    def test(self):
+        self.step_belt()
 
 def main(args):
     belt = Belt(args)
-    belt.run()
+    belt.test()
 
 if __name__ == "__main__":
     args = parse_args()
     classifier_setup(args)
     main(args)
-    #gpio_cleanup()
+    gpio_cleanup()
     classifier_cleanup()
