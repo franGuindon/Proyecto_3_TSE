171d7bbec923ab5fde96d13d24a73b8757841706
franGuindon <fbadilla10@gmail.com>
2022-05-30 17:31:41 -0600

    main.py obj_classifier.py: Lots of changes.

--- Programa_clasificador_objetos/main.py
+++ Programa_clasificador_objetos/main.py
@@ -1,20 +1,29 @@
+print("importing time")
 import time
+print("importing opencv")
+import cv2
 
+print("importing parser")
 from arg_parser import parse_args
+print("importing gpio")
 from gpio import Pin, cleanup as gpio_cleanup
+
+print("importing classifier")
 from obj_classifier import \
+    grab_fame, \
     setup as classifier_setup, \
     classify_object, try_classify_object, \
-    cleanup as classifier_cleanup
+    cleanup as classifier_cleanup, \
+    continuosly_classify_object
 
 class Belt:
 
     def __init__(self, args):
         self.args = args
-        self.setup_pins()
-
+        #self.setup_pins()
+    
     def setup_pins(self):
-        self.belt_motor_pins = Pin(13), Pin(11), Pin(15), Pin(12)
+        self.belt_motor_pins = Pin(31), Pin(33), Pin(35), Pin(37)
         #self.arm_motor = Pin(1)
 
     def write_belt(self, seq):
@@ -30,15 +39,13 @@ class Belt:
         if direction: index_range = range(0, self.args.step_count, 1)
         else: index_range = range(-self.args.step_count, 0, -1)
         for i in index_range:
-            elem = seq[i]
+            elem = seq[i%8]
             self.write_belt(elem)
             time.sleep(self.args.velocity)
 
     def place_object_at_camera(self):
-        grabbed = False
-        while not grabbed:
-            self.step_belt()
-            grabbed, self.obj_class = try_classify_object()
+        self.step_belt()
+        
 
     def classify_object(self): pass
     
@@ -49,15 +56,22 @@ class Belt:
             self.classify_object()
     
     def test(self):
-        self.step_belt()
+        continuosly_classify_object()
 
 def main(args):
     belt = Belt(args)
-    belt.test()
+    belt.run()
 
 if __name__ == "__main__":
+    print("running main")
+    print("parsing args")
     args = parse_args()
+    print("debug mode set to", args.debug)
+    print("setting up classifier")
     classifier_setup(args)
+    print("running main obj")
     main(args)
+    print("gpio cleanup")
     gpio_cleanup()
+    print("classifier cleanup")
     classifier_cleanup()
--- Programa_clasificador_objetos/obj_classifier.py
+++ Programa_clasificador_objetos/obj_classifier.py
@@ -1,13 +1,16 @@
 import cv2
 import numpy as np
-from ternsorflow.interpreter import Interpreter
+
+from tflite_runtime.interpreter import Interpreter
 
 MODEL_FILE = "detect.tflite"
 
+print("Creating interpreter")
 INTERPRETER = Interpreter(model_path=MODEL_FILE)
-
+print("Allocating tensors")
 INTERPRETER.allocate_tensors()
 
+print("Getting input and output details")
 INPUT_DETAILS = INTERPRETER.get_input_details()[0]
 BOXES_DETAILS = INTERPRETER.get_output_details()[0]
 SCORES_DETAILS = INTERPRETER.get_output_details()[2]
@@ -17,14 +20,16 @@ WIDTH = INPUT_DETAILS['shape'][2]
 
 FLOATING_MODEL = (INPUT_DETAILS['dtype'] == np.float32)
 
+print("Setting up video capture")
 CAMERA = cv2.VideoCapture(0)
+print("Setting up video format to MJPG")
 CAMERA.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
 
 def setup(args):
     global ARGS
     ARGS = args
-    CAMERA.set(cv2.CAP_PROP_FRAME_WIDTH, ARGS.width)
-    CAMERA.set(cv2.CAP_PROP_FRAME_HEIGHT, ARGS.height)
+    # CAMERA.set(cv2.CAP_PROP_FRAME_WIDTH, ARGS.width)
+    # CAMERA.set(cv2.CAP_PROP_FRAME_HEIGHT, ARGS.height)
 
 def get_boxes_and_scores(frame):
     frame_copy = frame.copy()
@@ -74,37 +79,57 @@ def show_avg_color_box(frame, top_left, bot_right, avg_color):
     cv2.imshow('Debug window', frame)
 
 def show_class_and_score(frame, top_left, bot_right, obj_class, score):
+    print("showing class and score")
     cv2.rectangle(frame, top_left, bot_right, (10, 255, 0), 2)
     label = f"Class: {obj_class}, Score: {score*100:.0f}"
     cv2.putText(frame, label, top_left, cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
     cv2.imshow('Debug window', frame)
 
+def grab_fame():
+    ret = True
+    while ret:
+        ret, frame = CAMERA.read()
+        cv2.imshow("", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'): break
+    cv2.destroyAllWindows()
+    CAMERA.release()
+
 def try_classify_object():
+    print("grabbing frame")
     grabbed, frame = CAMERA.read()
     if not grabbed: return False, None
-
+    print("getting obj boxes and scores")
+    """
     boxes, scores = get_boxes_and_scores(frame)
     (ymin, xmin, ymax, xmax), score = get_main_dims_and_score(boxes, scores)
     if not (ymin or xmin or ymax or xmax): return False, None
-
+    print("cropping img")
     frame_cropped = frame[ymin:ymax, xmin:xmax]
     avg_color = np.average(frame_cropped, axis=(0,1))
+    """
+    avg_color = np.average(frame, axis=(0,1))
     obj_class = get_obj_class(avg_color)
+    print("running debug code")
     if ARGS.debug:
         #show_avg_color_box(frame, (xmin, ymin), (xmax, ymax), avg_color)
-        show_class_and_score(frame, (xmin, ymin), (xmax, ymax), obj_class, score)
-        if cv2.waitKey(1) == ord('q'): return False, None
+        #show_class_and_score(frame, (xmin, ymin), (xmax, ymax), obj_class, score)
+        cv2.imshow("", frame)
+        if cv2.waitKey(1) == ord('q'): return False, "break"
     return True, obj_class
 
-def classify_object():
-    grabbed = False
-    obj_class = None
-    while not grabbed: grabbed, obj_class = try_classify_object()
-    return obj_class
+def classify_object(attempts):
+    array = []
+    while len(array) < attempts:
+        grabbed, obj_class = try_classify_object()
+        if grabbed: array.append[obj_class]
+    obj_class = np.average(array)
+    return int(round(obj_class, 0))
 
 def continuosly_classify_object():
-    grabbed = True
-    while grabbed: grabbed, _ = try_classify_object()
+    while True:
+        grabbed, obj_class = try_classify_object()
+        print(obj_class)
+        if obj_class == "break": break
 
 def cleanup():
     cv2.destroyAllWindows()
